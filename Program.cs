910fd83fe534c17f646ca22bd5c57b706536bac2
Prathamesh Rajput <rajputprathamesh1998@gmail.com>
2020-10-10 15:02:47 +0530

    [Prathamesh Rajput] Add. Completed UseCase1

--- Program.cs
+++ Program.cs
@@ -8,9 +8,40 @@ namespace TicTacToe
 {
     class Program
     {
+
+        static char[] board = new char[10];
+
+        /// <summary>
+        /// Displays the board.
+        /// </summary>
+        static void DisplayBoard()
+        {
+            Console.WriteLine("    "+board[1] + "|" + board[2] + "|" + board[3]);
+            Console.WriteLine("   -------");
+            Console.WriteLine("    " + board[4] + "|" + board[5] + "|" + board[6]);
+            Console.WriteLine("   -------");
+            Console.WriteLine("    " + board[7] + "|" + board[8] + "|" + board[9]);
+        }
+
+
+        /// <summary>
+        /// Starts the game.
+        /// </summary>
+        static void StartGame()
+        {
+            for (int i = 1; i < 10; i++)
+                board[i] = ' ';
+
+            Console.WriteLine("New Game Started");
+            DisplayBoard();
+        }
+
         static void Main(string[] args)
         {
-            Console.Write("Welcome to Tic Tac Toe Game");
+            StartGame();
+
+            //Temp ReadLine to view Console
+            Console.ReadLine();
         }
     }
 }
